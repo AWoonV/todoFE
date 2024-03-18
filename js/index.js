686bf873b0817123b122085a514d64bf6e4898d4
Woon <143389871+AWoonV@users.noreply.github.com>
2024-03-19 00:38:19 +0200

    changing link correctly this time T__T

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const BACKEND_ROOT_URL = 'https://todo-backend-4o33.onrender.com/'
+const BACKEND_ROOT_URL = 'https://todo-backend-4o33.onrender.com'
 import { Todos } from "./class/Todos.js"
 
 const todos = new Todos(BACKEND_ROOT_URL)
